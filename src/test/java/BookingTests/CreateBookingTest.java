be721c2ed74d5217e8a787fe0840829854abff25
regiscalheira <regiscalheira@gmail.com>
2022-05-11 00:19:20 -0300

    beginning the repository

---
+++ src/test/java/BookingTests/CreateBookingTest.java
@@ -0,0 +1,10 @@
+package Modules.CreateBooking;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("API Rest Tests of the Create Booking Module")
+public class CreateBookingTest {
+    @Test
+    @DisplayName("Validate the Creation of a new Booking")
+}
